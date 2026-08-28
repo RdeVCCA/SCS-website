56ef2f7ee27924cd455e535203a18c8db3ce29a9
vensah-dev <venkateshdeven14@gmail.com>
2026-08-28 16:04:00 +0800

    uhh changed for ut to work on github pages??

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,14 @@ import type { NextConfig } from 'next';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  images: {
-    formats: ['image/avif', 'image/webp'],
-  },
-  // output: 'export',
-  // basePath: isProd ? `/y-lead-2026` : '',
   // images: {
-  //   unoptimized: true,
+  //   // formats: ['image/avif', 'image/webp', 'image/png', 'image/jpeg'],
   // },
+  output: 'export',
+  basePath: isProd ? `/y-lead-2026` : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
